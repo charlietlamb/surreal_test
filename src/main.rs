2175d7d6b0fe68f6114a94b1bab6e38829967bd5
Charlie Lamb <charlielamb20@icloud.com>
2024-09-15 13:30:37 +0100

    attempitng connection

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+use serde::{Deserialize,Serialize};
+use surrealdb::engine::remote::ws::Ws;
+use surrealdb::opt::auth::Root;
+use surrealdb::sql::Thing;
+use surrealdb::Surreal;
+
+#[derive(Debug, Serialize)]
+struct Name<'a> {
+    first: &'a str,
+    last: &'a str,
+}
+
+#[derive(Debug, Serialize)]
+struct Person<'a> {
+    title: &'a str,
+    name: Name<'a>,
+    marketing: bool,
+}
+
+#[derive(Debug, Serialize)]
+struct Responsibility {
+    marketing: bool,
+}
+
+#[derive(Debug, Deserialize)]
+struct Record {
+    #[allow(dead_code)]
+    id: Thing,
+}
+
+#[tokio::main]
+async fn main() -> surrealdb::Result<()> {
+    //Connect to the server
+    let db = Surreal::new::<Ws>("127.0.0.1:8000").await?;
+    db.signin(Root {
+        username: "root",
+        password: "root",
+    }).await?;
+    
+    Ok(())
+}
